b6f5d176051b1b3971c1cd61764491d7f82f0927
User <User@DESKTOP-55OTKCO>
2023-11-02 14:20:21 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Core/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace WpfApp1.Core
+{
+    class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public ICommand CreateCommand { get {
+            return _createCommand  (_createCommand=new RelayCommand(param=>CreateNewCommand()))}
+        }
+        public event PropertyChagedEventHandler? PropertyChanged;
+    }
+}
---
+++ WpfApp1/Core/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.Core
+{
+    class RelayCommand: ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        
+}
+}
